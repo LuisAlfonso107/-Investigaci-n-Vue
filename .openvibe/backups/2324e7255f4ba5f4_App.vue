fdf0f56538075aded4124d020abf3de98a388dcf
LuisAlfonso107 <luisalfonsogarcialagos9@gmail.com>
2026-01-27 14:15:07 +0100

    comit

--- .openvibe/backups/2324e7255f4ba5f4_App.vue
+++ .openvibe/backups/2324e7255f4ba5f4_App.vue
@@ -4,23 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
 
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/investigacion-reactiva">Investigación Reactividad</RouterLink>
-        </nav>
-      </div>
-    </header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/investigacion-reactiva">Investigación Reactividad</RouterLink>
+      </nav>
+    </div>
+  </header>
 
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
