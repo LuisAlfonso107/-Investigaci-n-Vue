d016528bcadabdb61c57e3e2cd963aac05b2f1e5
yggabo <hernandezcruz.gabrielosmin@gmail.com>
2026-02-03 12:37:30 +0100

    APIs

--- Investigacion-VUE/src/views/ContactView.vue
+++ Investigacion-VUE/src/views/ContactView.vue
@@ -1,31 +1,24 @@
 <template>
   <div class="contact">
-    <h1>ejmplo</h1>
-    <p>Esto es un ejemplo de una vista</p>
+    <OptionApi />
+    <!-- <CompositonApiOpcionUno /> -->
+    <!-- <CompositonApiOpcionDos /> -->
   </div>
 </template>
 
-<script>
-export default {
-  name: 'ContactView'
-}
+<script setup>
+  import OptionApi from '../components/OptionApi.vue'
+  // import CompositonApiOpcionUno from '../components/CompositonApiOpcionUno.vue'
+  // import CompositonApiOpcionDos from '../components/CompositionApiOpcionDos.vue'
 </script>
 
-<style scoped>
-.contact {
-  text-align: center;
-  padding: 2rem;
-  max-width: 1200px;
-  margin: 0 auto;
-}
-
-h1 {
-  font-size: 2.5rem;
-  margin-bottom: 1rem;
-}
-
-p {
-  font-size: 1.2rem;
-  color: var(--color-text);
-}
+<style>
+  .contact {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: white;
+    margin-top: 60px;
+  }
 </style>
